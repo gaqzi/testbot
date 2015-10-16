7806bee6a7f9c6c38b351b33caadc8bd2dfd3d05
Björn Andersson <bjorn@neo.com>
2015-10-16 22:41:35 +0800

    wip

---
+++ test/shared/adapters/tagged_cucumber_adapter_test.rb
@@ -0,0 +1,9 @@
+require File.expand_path(File.join(File.dirname(__FILE__), '../../../lib/shared/adapters/tagged_cucumber_adapter.rb'))
+require 'test/unit'
+require 'shoulda'
+
+class TaggedCucumberAdapterTest < Test::Unit::TestCase
+  context 'test_files' do
+    
+  end
+end
